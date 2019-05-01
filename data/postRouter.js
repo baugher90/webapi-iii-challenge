f298737b52a3e200c5b941833885fd66ccf501f9
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-01 16:03:42 -0400

    Generic PostRouter set up

---
+++ data/postRouter.js
@@ -0,0 +1,9 @@
+const express =require("express");
+const Post = require("./helpers/postDb")
+
+const router = express.Router();
+//=================================================Post Routers
+//=================================================Get Routers
+//=================================================Update Routers
+//=================================================Delete Routers
+module.exports = router
