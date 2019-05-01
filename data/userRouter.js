4d5b210c66e4b4b91adcef683af21946dc77938f
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-01 16:03:05 -0400

    Generic UserRouter set up

---
+++ data/userRouter.js
@@ -0,0 +1,9 @@
+const express =require("express");
+const User = require("./helpers/userDb")
+
+const router = express.Router();
+//=================================================Post Routers
+//=================================================Get Routers
+//=================================================Update Routers
+//=================================================Delete Routers
+module.exports = router
