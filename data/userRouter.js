7363d1fc92834454915ec8f3d090d1a57a8af55d
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-01 16:06:19 -0400

    Generic router methods are set in postRouter and userRouter

--- data/userRouter.js
+++ data/userRouter.js
@@ -3,7 +3,12 @@ const User = require("./helpers/userDb")
 
 const router = express.Router();
 //=================================================Post Routers
+router.post("/",(req,res)=>{})
 //=================================================Get Routers
+router.get("/",(req,res)=>{})
 //=================================================Update Routers
+router.put("/",(req,res)=>{})
 //=================================================Delete Routers
+router.delete("/",(req,res)=>{})
+
 module.exports = router
